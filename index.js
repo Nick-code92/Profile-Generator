e8858e354e81770d7d72499fb2d1b40e08603e0f
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2020-01-14 23:37:22 -0700

    new folders

---
+++ index.js
@@ -0,0 +1,17 @@
+const genHTML = require("./generateHTML")
+const questions = [
+  
+];
+var data = {
+    color: "green"
+}
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+    console.log(genHTML(data))
+}
+
+init();
